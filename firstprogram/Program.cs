5f4d4c2acbf5df6952bb5916115e05624366a328
nesrinemenifie <faydoennour@gmail.com>
2018-02-16 19:20:32 +0100

    add a comment

--- firstprogram/Program.cs
+++ firstprogram/Program.cs
@@ -10,6 +10,8 @@ namespace firstprogram
     {
         static void Main(string[] args)
         {
+           //this is my first program
+
             Console.WriteLine("Hello World");
             Console.ReadKey();
         }
